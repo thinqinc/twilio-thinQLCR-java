311bbf6e89c83b68c82541a9348139fcc7c6b6fd
Ben Conley <ben.conley@playmakercrm.com>
2017-04-19 11:04:45 -0400

    Add Maven support and fix some incorrect pathing, naming, unnecessary jars, etc.

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# twilio-thinQLCR-java, Twilio Wrapper Java Library For thinQ LCR integration
+### twilio-thinQLCR-java, Twilio Wrapper Java Library For thinQ LCR integration
 
-##**Note that you will need a valid LCR Account with thinQ before using the libraries. For more information please contact your thinQ Sales representative at [http://www.thinq.com/library/](http://www.thinq.com/library/)**
+**Note that you will need a valid LCR Account with thinQ before using the libraries. For more information please contact your thinQ Sales representative at [http://www.thinq.com/library/](http://www.thinq.com/library/)**
 
 #### *Copyright (c) 2016 thinQ*
+---
+Download public key:
+``` javascript
+gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys F369A178F1C54CCB93F540EAC55F351BC8CCD7F4
+```
+
+Verify signatures:
+```
+gpg2 --verify pom.xml.asc
+```
---
+++ src/main/java/com/twilio/thinq/TwilioWrapperLibrary.java
@@ -0,0 +1,77 @@
+package com.twilio.thinq;
+
+import com.twilio.sdk.TwilioRestClient;
+import com.twilio.sdk.TwilioRestException;
+import com.twilio.sdk.resource.factory.CallFactory;
+import com.twilio.sdk.resource.instance.Call;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class TwilioWrapperLibrary {
+
+    private static final String THINQ_DOMAIN = "wap.thinq.com";
+
+    private TwilioRestClient client;
+    private String twilio_account_sid;
+    private String twilio_account_token;
+    private String thinQ_id;
+    private String thinQ_token;
+
+
+    public TwilioWrapperLibrary(){
+
+    }
+
+    /**
+     * Initialize Twilio Wrapper Object
+     * @param twilio_account_sid    twilio account sid
+     * @param twilio_account_token  twilio account token
+     * @param thinQ_id      thinQid received when signed up at thinQ website
+     * @param thinQ_token   thinQtoken received when signed up at thinQ website
+     */
+
+    public TwilioWrapperLibrary(String twilio_account_sid, String twilio_account_token, String thinQ_id, String thinQ_token) {
+
+        this.twilio_account_sid = twilio_account_sid;
+        this.twilio_account_token = twilio_account_token;
+        this.thinQ_id = thinQ_id;
+        this.thinQ_token = thinQ_token;
+
+        this.client = new TwilioRestClient(this.twilio_account_sid, this.twilio_account_token);
+    }
+
+    /**
+     * Check if the twilio client is initialized properly.
+     */
+    public boolean isClientValid(){
+        return this.client != null && this.client.getAccount() != null;
+    }
+
+    /**
+     * Initiate a call to the customer
+     * pass through of ArrayList of NameValuePairs that Twilio object expects
+     */
+
+    public Call call(ArrayList<NameValuePair> params) throws TwilioRestException {
+        CallFactory callFactory = this.client.getAccount().getCallFactory();
+        Call call;
+
+        // ensure the To field is properly formatted
+        String to_number;
+        ArrayList<NameValuePair> twilioparams = new ArrayList<NameValuePair>();
+        for (NameValuePair temp : params) {
+            if (Objects.equals(temp.getName(), "To")) {
+                to_number = temp.getValue().startsWith("sip:") ? temp.getValue() : "sip:" + temp.getValue() + "@" + THINQ_DOMAIN + "?thinQid=" + thinQ_id + "&thinQtoken=" + thinQ_token;
+                twilioparams.add(new BasicNameValuePair("To", to_number));
+            } else {
+                twilioparams.add(new BasicNameValuePair(temp.getName(), temp.getValue()));
+            }
+        }
+
+        call = callFactory.create(twilioparams);
+        return call;
+    }
+}
